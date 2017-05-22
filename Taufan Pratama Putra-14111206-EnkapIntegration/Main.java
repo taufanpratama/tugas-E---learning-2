9e248d77badd7684768ee4f16160c7ab22aafede
Taufan Pratama Putra <taufanpratamaputra22@gmail.com>
2017-05-22 22:07:09 +0700

    Add files via upload

---
+++ Taufan Pratama Putra-14111206-EnkapIntegration/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ruangan_kelas data= new ruangan_kelas("Meja Belajar","Kursi Tempat Duduk");
+        System.out.println(" Attribut ruangan Kelas : " +data.setmeja());
+        System.out.println(" Attribut ruangan Kelas : " +data.kursi);
+}
+}
---
+++ Taufan Pratama Putra-14111206-EnkapIntegration/ruangan_kelas.java
@@ -0,0 +1,21 @@
+public class ruangan_kelas {
+        private String meja;
+        public  String kursi;
+   
+public ruangan_kelas (String meja, String kursi){
+         
+     this.meja  = meja;
+     this.kursi = kursi;
+     
+ }
+     
+     public String setmeja(){
+         return meja;
+     }
+     public String setkursi(){
+         return meja; 
+   
+}
+
+}      
+       
